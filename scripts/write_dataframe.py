67e37baf72a19c15e07373b820e131e5ec7f474e
Timothy Morton <tdm@astro.princeton.edu>
2018-12-02 00:07:31 -0500

    more nb

--- scripts/write_dataframe.py
+++ scripts/write_dataframe.py
@@ -23,7 +23,8 @@ columns = ['J', 'J_ERR', 'H', 'H_ERR', 'K', 'K_ERR', 'source_id2', 'ref_epoch', 
            'phot_bp_mean_flux_over_error', 'phot_bp_mean_mag', 'phot_rp_n_obs', 'phot_rp_mean_flux', 'phot_rp_mean_flux_error',
            'phot_rp_mean_flux_over_error', 'phot_rp_mean_mag', 'IRAC_3_6', 'IRAC_3_6_ERR', 'IRAC_4_5', 'IRAC_4_5_ERR',
            'IRAC_5_8', 'IRAC_5_8_ERR', 'IRAC_8_0', 'IRAC_8_0_ERR', 'w1mpro', 'w1mpro_error', 'w2mpro', 'w2mpro_error',
-           'w3mpro', 'w3mpro_error', 'w4mpro', 'w4mpro_error', 'tmass_key', 'spec_parallax', 'spec_parallax_err']
+           'w3mpro', 'w3mpro_error', 'w4mpro', 'w4mpro_error', 'tmass_key', 'spec_parallax', 'spec_parallax_err',
+           'TEFF', 'TEFF_ERR', 'LOGG', 'LOGG_ERR', 'M_H', 'M_H_ERR']
 
 rgb_df = rgbs[columns].to_pandas()
 
@@ -50,16 +51,18 @@ rgb_df['G_unc'] = (2.5 / np.log(10)) * G_flux_unc/G_flux
 rgb_df['BP_unc'] = (2.5 / np.log(10)) * BP_flux_unc/BP_flux
 rgb_df['RP_unc'] = (2.5 / np.log(10)) * RP_flux_unc/RP_flux
 
-band_map = dict(J='J', H='H', K='K',
+col_map = dict(J='J', H='H', K='K',
                 w1mpro='W1', w2mpro='W2', w3mpro='W3', w4mpro='W4',
-                IRAC_3_6='IRAC_3.6', IRAC_4_5='IRAC_4.5', IRAC_5_8='IRAC_5.8', IRAC_8_0='IRAC_8.0')
+                IRAC_3_6='IRAC_3.6', IRAC_4_5='IRAC_4.5', IRAC_5_8='IRAC_5.8', IRAC_8_0='IRAC_8.0',
+              TEFF='Teff', LOGG='logg', M_H='feh',
+              parallax_error='parallax_unc')
 
 for c in rgb_df.columns:
     m = re.search('(\w+)_[Ee][Rr][Rr]', c)
     if m:
-        if m.group(1) in band_map:
-            band_map.update({c: '{}_unc'.format(band_map[m.group(1)])})
+        if m.group(1) in col_map:
+            col_map.update({c: '{}_unc'.format(col_map[m.group(1)])})
 
-rgb_df.rename(columns=band_map, inplace=True)
+rgb_df.rename(columns=col_map, inplace=True)
 
 rgb_df.to_hdf(os.path.join(ROOT, 'rgb.hdf'), 'df')
